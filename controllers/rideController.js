bcf020919f879d49c4175b58e52aae194e9d2233
Sriram277 <sriramakiran727@gmail.com>
2017-02-14 16:07:38 +0530

    smtp added

--- controllers/rideController.js
+++ controllers/rideController.js
@@ -10,7 +10,10 @@ var mongoose = require('mongoose');
 var Ride = mongoose.model('Ride');
 var User = mongoose.model('User');
 var nodemailer = require('nodemailer');
-var transporter_smtp = nodemailer.createTransport(config.EMAIL_CREDENTIALS);
+var transport = nodemailer.createTransport(config.EMAIL_CREDENTIALS);
+var smtpTransport = require('nodemailer-smtp-transport');
+
+var transporter_smtp = nodemailer.createTransport(smtpTransport(config.EMAIL_CREDENTIALS));
 
 module.exports = {
 
